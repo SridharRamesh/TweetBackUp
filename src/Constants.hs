82e0020cdd559d6009fa7141cc60a77d88433f8f
Sridhar Ramesh <chinju@gmail.com>
2021-10-07 12:39:41 -0400

    Actually commit all files

---
+++ src/Constants.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE 
+  OverloadedStrings,
+  NoMonomorphismRestriction
+#-}
+module Constants where
+
+import System.Directory
+
+generateInputPath = canonicalizePath "../../raw/FullTweetArchive.txt"
+generateOutputDirectory = canonicalizePath "../../archive/"
+
+
+-- Personal constants
+displayName = "Sridhar Ramesh"
+atName = "RadishHarmers"
+
+-- Frequently reconfigured constants
+printForDate year month day = year == 2020
+-- printForDate year month day = True
